47142e188b0a3f576c9b0b7294a82e44faa86e6c
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2025-11-26 00:01:48 +0545

    utils code discussion and api error handling

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 import { DB_NAME } from "./constant.js"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+// app()
+
+connectDB() //async promise to return something so(.then() and .cath())
+.then(()=>{  // for success
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`The server is running on the port number ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{    // database connection fail...
+    console.log("MONGODB connection failed!!!!",err)
+})
 
